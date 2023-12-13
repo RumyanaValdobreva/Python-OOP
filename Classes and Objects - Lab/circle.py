17e0149dcb403f763283851b7c2c28f094b599d8
Rumyana Valdobreva <rumyiwanowa@abv.bg>
2023-12-13 16:47:20 +0200

    Adding Classes and Objects - Lab

---
+++ Classes and Objects - Lab/circle.py
@@ -0,0 +1,15 @@
+class Circle:
+    pi = 3.14
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def set_radius(self, new_radius):
+        self.radius = new_radius
+
+    def get_area(self):
+        result = Circle.pi * self.radius ** 2
+        return result
+    def get_circumference(self):
+        result = 2 * Circle.pi * self.radius
+        return result
